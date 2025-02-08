f0b49fb1d0f1b3a0b8208772a5879f6339f49cf5
2596907696@qq.com <2596907696@qq.com>
2025-02-08 11:59:15 +0800

    Site updated: 2025-02-08 11:59:15

---
+++ custom.js/custom.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-subtitle", {
+        strings: ["������1", "������2", "������3"], // ��ϣ��չʾ�ĸ���������
+        typeSpeed: 100, // �����ٶ�
+        backSpeed: 50, // ɾ���ٶ�
+        backDelay: 1000, // ����ǰ�ȴ�ʱ��
+        startDelay: 500, // ��ʼǰ�ȴ�ʱ��
+        loop: true // �Ƿ�ѭ������
+    });
+});
---
+++ js/custom.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-subtitle", {
+        strings: ["������1", "������2", "������3"], // �滻�����Լ��ĸ���������
+        typeSpeed: 100, // �����ٶ�
+        backSpeed: 50, // ɾ���ٶ�
+        backDelay: 1000, // ɾ������ӳ�ʱ��
+        startDelay: 500, // ��ʼ����ǰ���ӳ�
+        loop: true // �Ƿ�ѭ������
+    });
+});
